cf15e74e7ac306fa87607b4c5455dd94dfe23440
ingeniero26 <ingjerson2014@gmail.com>
2025-04-28 10:59:50 -0500

    lista generica para listar tablas

---
+++ OrderApp/OrderApp.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using OrderApp.Frontend.Repositorios;
+using OrderApp.Shared.Entities;
+
+namespace OrderApp.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        public List<Country>? Countries { get; set; } = new List<Country>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                var responseHttp = await Repository.GetAsync<List<Country>>("api/countries");
+                Countries = responseHttp.Response;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cargar los datos: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ OrderApp/OrderApp.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace OrderApp.Frontend.Shared
+{
+    public partial class GenericList<TItem>
+    {
+        [Parameter]  public RenderFragment? Loading { get; set; }
+        [Parameter]  public RenderFragment? NoRecords { get; set; }
+        [EditorRequired, Parameter] public RenderFragment Body { get; set; } = null!;
+        [EditorRequired, Parameter] public List<TItem> MyList { get; set; } = null!;
+
+
+    }
+}
